52d6daec8e72c08185f6bf8f2b205617614a36ad
Maxime liege <liege30@gmail.com>
2018-12-03 09:56:59 +0100

    change port to 80

--- controllers/server.js
+++ controllers/server.js
@@ -2,7 +2,7 @@ const logger = require("./logger");
 const express = require("express");
 const app = express();
 
-const port = 3300;
+const port = 80;
 
 const server = app.listen(port, () => {
   logger.info(`Server is launched on ${port} port`);
